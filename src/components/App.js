445bc693d6961814dc7c647f288abe4e17597d24
Sam Hall <mann.samantha11@gmail.com>
2021-02-27 17:50:08 -0500

    commit tree 01cb53b9579a25e25ab07ef5f35fba5359b5bf02

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+
+  const[plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(data => setPlants(data))
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} setPlants={setPlants}/>
     </div>
   );
 }
